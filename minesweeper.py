938e8ba89ca75641077f77c998424caec2a635c8
peterferreira <peter.e.ferreira@gmail.com>
2023-11-20 17:04:12 -0500

    Fixed safe move

--- minesweeper.py
+++ minesweeper.py
@@ -1,4 +1,4 @@
-# import itertools
+import itertools
 import random
 
 
@@ -231,7 +231,7 @@ class MinesweeperAI():
         and self.moves_made, but should not modify any of those values.
         """
         for cell in self.safes:
-            if cell in self.moves_made:
+            if cell not in self.moves_made and cell not in self.mines and cell in self.safes:
                 return cell
         return None
 
